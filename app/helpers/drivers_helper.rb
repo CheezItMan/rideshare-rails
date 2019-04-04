618b99a87fdd9df6c94a0f9ab48ab3a19812f7c6
CheezItMan <chris@adadevelopersacademy.org>
2019-04-03 22:12:06 -0700

    update helpers

--- app/helpers/drivers_helper.rb
+++ app/helpers/drivers_helper.rb
@@ -1,11 +1,11 @@
 module DriversHelper
   def driver_activate_button(driver)
     if driver.active?
-      text = 'Go Offline'
-      style = 'button alert'
+      text = "Go Offline"
+      style = "btn btn-danger"
     else
-      text = 'Go Online'
-      style = 'button success'
+      text = "Go Online"
+      style = "btn btn-primary"
     end
 
     button_to text, activate_driver_path(driver), method: :post, class: style
